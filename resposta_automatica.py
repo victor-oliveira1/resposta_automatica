0f883c6befb82768e13fb5e00fd0ebf5e0363f00
Victor Oliveira <victor.oliveira@gmx.com>
2017-09-11 08:32:21 -0300

    Update resposta_automatica.py
    Adicionado histórico de versões.

--- resposta_automatica.py
+++ resposta_automatica.py
@@ -31,6 +31,10 @@ Exemplo (Desativar em uma linha):
 >>> import resposta_automatica; RespostaAutomatica('email@email.com.br', 'senha_secreta').Desativar()
 >>> True
 
+Histórico de versões:
+Versão 1.0.1: Primeiro release. Adicionado orientação à objeto. Duas funções adicionadas.
+Versão 1.0.0: Criado para testes internos e estudo de funcionamento, não publicado.
+
 Versão: 1.0.1
 Escrito por Victor Oliveira
 victor.oliveira@gmx.com
